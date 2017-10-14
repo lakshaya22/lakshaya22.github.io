5106d8eaa71b2bc2242fe5de17c991564e2a4501
lakshaya22 <lakshaya.maheshwari22@gmail.com>
2017-10-15 00:47:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,16 +20,23 @@ controller("TreeController", ['$scope', function($scope) {
       {level:1,name: "Company",designation:'',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: [
         {level:2,name: "Rentomojo",designation:'Ceo',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false,nodes: [
           {level:3,name: "Core",designation:'Cfo',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: [
-            {level:4,name: "Core",designation:'Cfo',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
-            {level:4,name: "Kyc",designation:'EA',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
-            {level:4,name: "Core",designation:'Vp Core',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
-            {level:4,name: "Kyc",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []}
+            {level:4,name: "Core1",designation:'Cfo',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Core2",designation:'EA',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Core3",designation:'Vp Core',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Core4",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []}
           ]},
-          {level:3,name: "Kyc",designation:'EA',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+          {level:3,name: "Kyc",designation:'EA',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: [
+            {level:4,name: "Kyc1",designation:'Cfo',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Kyc2",designation:'EA',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Kyc3",designation:'Vp Core',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Kyc4",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Kyc5",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
+            {level:4,name: "Kyc6",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []}
+          ]},
           {level:3,name: "Core",designation:'Vp Core',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []},
           {level:3,name: "Kyc",designation:'Vp Kyc',personName:'Daniel',employee:'4',reportEmployee:'3',showChild:false, nodes: []}
 
         ]}           
     ]}];
-    
+
 }]);
